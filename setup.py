0e26e553ed3c38b77751b185ce08f7f79a89a2cc
futzu <52701496+futzu@users.noreply.github.com>
2019-11-09 22:19:58 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="threefive", # Replace with your own username
-    version="1.0.7e",
+    version="1.0.91",
     author="fu-corp",
     author_email="spam@futzu.com",
     description="scte 35 parsing tools",
