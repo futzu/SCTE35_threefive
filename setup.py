b6fb1fb991e3f8ed91d757db90533c5fbe056a1c
futzu <52701496+futzu@users.noreply.github.com>
2019-11-12 08:29:33 -0500

    version bumped to 1.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="threefive", 
-    version="1.1.3",
+    version="1.1.5",
     author="fu-corp",
     author_email="spam@futzu.com",
     description="scte 35 parsing tools",
