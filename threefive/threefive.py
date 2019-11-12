a5930e39d1fa1069606efbe3029b8eef470f7bec
futzu <52701496+futzu@users.noreply.github.com>
2019-11-12 08:58:02 -0500

    self.cw_index = hex(bb.read('uint:8'))

--- threefive/threefive.py
+++ threefive/threefive.py
@@ -284,7 +284,7 @@ class Splice_Info_Section:
         self.encrypted_packet =  bb.read('bool')
         self.encryption_algorithm =bb.read('uint:6')
         self.pts_adjustment = time_90k(bb.read('uint:33'))
-        self.cw_index = bb.read('uint:8').hex
+        self.cw_index = hex(bb.read('uint:8'))
         self.tier = hexed(bb.read('uint:12'))
         self.splice_command_length = bb.read('uint:12')
         self.splice_command_type = bb.read('uint:8')
