ea24eece0e2f886c54d2bf2e2550cd469a087237
futzu <52701496+futzu@users.noreply.github.com>
2019-11-18 17:49:16 -0500

    table22

--- threefive/threefive.py
+++ threefive/threefive.py
@@ -1,5 +1,68 @@
 import base64,bitstring
 
+
+def not_zero(i):
+    return i !=0
+
+def gte_zero(i):
+    return i >=0
+
+'''
+table 22 from page 62 of 
+https://www.scte.org/SCTEDocs/Standards/ANSI_SCTE%2035%202019r1.pdf
+I am using the segmentation_type_id as a key.
+
+Segmentation_type_id = [segmentation_message,
+			segment_num,
+			segments_expected,
+			sub_segment_num,
+			sub_segments_expected]
+'''
+table22={
+0x00  : [ "Not Indicated",0,0, None,None],
+0x01  : [ "Content Identification",0,0, None,None],
+0x10  : [ "Program Start",1,1, None,None],
+0x11  : [ "Program End",1,1, None,None],
+0x12 : [ "Program Early Termination",1,1, None,None],
+0x13 : [ "Program Breakaway",1,1, None,None],
+0x14 : [ "Program Resumption",1,1, None,None],
+0x15  : [ "Program Runover Planned",1,1, None,None],
+0x16  : [ "Program RunoverUnplanned",1,1, None,None],
+0x17 : [ "Program Overlap Start",1,1, None,None],
+0x18  : [ "Program Blackout Override",0,0, None,None],
+0x19 : [ "Program Start – In Progress",1,1, None,None],
+0x20 : [ "Chapter Start",not_zero,not_zero, None,None],
+0x21 : [ "Chapter End",not_zero,not_zero, None,None],
+0x22 : [ "Break Start",gte_zero,gte_zero, None,None],
+0x23 : [ "Break End",gte_zero,gte_zero, None,None],
+0x24 : [ "Opening Credit Start",1,1, None,None],
+0x25 : [ "Opening Credit End",1,1, None,None],
+0x26 : [ "Closing Credit Start",1,1, None,None],
+0x27 : [ "Closing Credit End",1,1, None,None],
+0x30 : [ "Provider Advertisement Start",gte_zero,gte_zero, None,None],
+0x31 : [ "Provider Advertisement End",gte_zero,gte_zero, None,None],
+0x32 : [ "Distributor Advertisement Start",gte_zero,gte_zero, None,None],
+0x33 : [ "Distributor Advertisement End",gte_zero,gte_zero, None,None],
+0x34 : [ "Provider Placement Opportunity Start",gte_zero,gte_zero,gte_zero,gte_zero],
+0x35 : [ "Provider Placement Opportunity End",gte_zero,gte_zero, None,None],
+0x36 : [ "Distributor Placement Opportunity Start",gte_zero,gte_zero,gte_zero,gte_zero],
+0x37 : [ "Distributor Placement Opportunity End",gte_zero,gte_zero, None,None],
+0x38 : [ "Provider Overlay Placement Opportunity Start",gte_zero,gte_zero,gte_zero,gte_zero],
+0x39 : [ "Provider Overlay Placement Opportunity End",gte_zero,gte_zero, None,None],
+0x3A : [ "Distributor Overlay Placement Opportunity Start",gte_zero,gte_zero,gte_zero,gte_zero],
+0x3B : [ "Distributor Overlay Placement Opportunity End",gte_zero,gte_zero, None,None],
+0x40  : [ "Unscheduled Event Start",0,0, None,None],
+0x41  : [ "Unscheduled Event End",0,0, None,None],
+0x50  : [ "Network Start",0,0, None,None],
+0x51  : [ "Network End",0,0, None,None],
+0x3B : [ "Distributor Overlay Placement Opportunity End",gte_zero,gte_zero, None,None],
+0x40  : [ "Unscheduled Event Start",0,0, None,None],
+0x41  : [ "Unscheduled Event End",0,0, None,None],
+0x50  : [ "Network Start",0,0, None,None],
+0x51  : [ "Network End",0,0, None,None]}
+
+
+
 def hex_decode(k):
     try:
         return bytearray.fromhex(hex(k)[2:]).decode()
@@ -240,7 +303,7 @@ class Segmentation_Descriptor(Splice_Descriptor):
                 self.segmentation_upid_length=bb.read('uint:8')
                 self.turner_identifier=bb.read('bits:64')
             self.segmentation_type_id=bb.read('uint:8')
-            if self.segmentation_type_id in table22.keys():
+            if self.segmentation_type_id in tables.table22.keys():
                 self.segmentation_type= table22[self.segmentation_type_id][0]
             self.segment_num=bb.read('uint:8')
             self.segments_expected=bb.read('uint:8')
