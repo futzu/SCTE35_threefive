e1972e3049225ecff9f3f03eee37a38a501c1327
futzu <52701496+futzu@users.noreply.github.com>
2019-11-19 19:10:53 -0500

    Create Time_Signal–Placement_Opportunity_Start.py

---
+++ examples/Time_Signal–Placement_Opportunity_Start.py
@@ -0,0 +1,13 @@
+'''
+Example from the specification 
+
+14.1. Time_Signal–Placement_Opportunity_Start
+'''
+
+import threefive 
+
+Base64='/DA0AAAAAAAA///wBQb+cr0AUAAeAhxDVUVJSAAAjn/PAAGlmbAICAAAAAAsoKGKNAIAmsnRfg=='
+
+tf=threefive.Splice
+
+tf(Base64).show()
