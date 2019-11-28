7f852e512afa34de3148747e006082acc4335b76
futzu <52701496+futzu@users.noreply.github.com>
2019-11-28 09:58:46 -0500

    Update parse_mpegts.py

--- examples/parse_mpegts.py
+++ examples/parse_mpegts.py
@@ -1,26 +1,30 @@
 '''
 parse scte35 from mpegts binary files
 '''
-
-
 import sys, threefive
 from struct import unpack
 
-def parse_packet(packet):
-        sync, pid, count, payload = unpack('>BHB184s', packet)
-        pid = pid & 8191
-        if pid==0x135 and b'CUEI' in payload: 
-            cue=payload[1:]
-            threefive.Splice(cue).show()
+PACKET_SIZE=188
+PID=0x135
+SYNC_BYTE=0x47
+
+def parse_tsfile(tsfile):
+    with open(tsfile,'rb') as tsdata: parse_tsdata(tsdata)
+
+def parse_tsdata(tsdata):
+    while tsdata:
+        data = tsdata.read(PACKET_SIZE)
+        if not data: break
+        sync_offset = data.find(SYNC_BYTE)
+        if sync_offset != 0: data = data[sync_offset:]
+        sync,pid =unpack('>BH', data[:3])
+        pid=pid & 8191
+        if pid==PID: parse_tspacket(data[3:])
+
+def parse_tspacket(tspacket):
+    _, payload = unpack('>H183s', tspacket)
+    threefive.Splice(payload).show()
  
 if __name__ == '__main__': 
-    psize = 188  
-    with open(sys.argv[1],'rb') as tsfile:
-        while tsfile:
-            data = tsfile.read(psize)
-            if not data: break
-            sync_offset = data.find(0x47)
-            if sync_offset != 0: data = data[sync_offset:]
-            packet,data = data[:psize], data[psize:]
-            parse_packet(packet)
-
+    parse_tsfile(sys.argv[1])
+ 
