56e77ce05c474bfcebb232979e4c7f7044f2560b
futzu <52701496+futzu@users.noreply.github.com>
2019-11-28 12:24:09 -0500

    version bump to 1.1.39

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="threefive", 
-    version="1.1.37",
+    version="1.1.39",
     author="fu-corp",
     author_email="spam@futzu.com",
     description="scte 35 decoder ring",
