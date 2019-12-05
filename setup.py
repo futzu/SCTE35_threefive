eee7b4255eb101a21fd253e276603c102fe3fb0a
futzu <52701496+futzu@users.noreply.github.com>
2019-12-04 20:57:20 -0500

    1.1.57

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="threefive", 
-    version="1.1.55",
+    version="1.1.57",
     author="fu-corp",
     author_email="spam@futzu.com",
     description="scte 35 decoder ring",
