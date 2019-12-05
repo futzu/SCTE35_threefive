cd981ba3f84ec92fc73b7ece4e516520d8cba9c7
futzu <52701496+futzu@users.noreply.github.com>
2019-12-05 08:22:04 -0500

    Create stream.py

---
+++ threefive/stream.py
@@ -0,0 +1,35 @@
+from .threefive import *
+from .util import PACKET_SIZE
+from .splice import Splice
+from struct import unpack
+
+class Stream:
+    def __init__(self,tsfile=None,show_null=True):
+        self.splices=[]
+        self.PID=False
+        self.show_null=show_null
+        self.tsfille=4
+        self.parse_tsfile(tsfile)
+
+    def parse_tsfile(self,tsfile):
+        with open(tsfile,'rb') as tsdata:
+            while tsdata:
+                packet = tsdata.read(PACKET_SIZE)
+                if not packet: break
+                self.parse_tspacket(packet)
+
+    def parse_tspacket(self,packet):
+        sync, pid, _, cue = unpack('>BHH183s', packet)
+        pid= pid & 0x1fff
+        if self.PID:
+            if pid !=self.PID: return
+        if cue[0]==0xfc:
+            if cue[13]==0:
+                if not self.show_null: return 
+            try:
+                tf=Splice(cue)
+                if tf:
+                    tf.show()
+                    self.splices.append(tf)
+                    if not self.PID: self.PID=pid
+            finally: return
