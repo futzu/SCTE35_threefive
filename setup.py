44c5b11ac812520ddc2c424af854dc6fdee04c2f
CupCake of Doom <52701496+futzu@users.noreply.github.com>
2019-12-11 05:31:07 -0500

    1.1.63

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="threefive", 
-    version="1.1.61",
+    version="1.1.63",
     author="fu-corp",
     author_email="spam@futzu.com",
     description="scte 35 decoder ring",
