ef511abd70cf82b4cff6a2363ed19fa4eaadf255
CupCake of Doom <52701496+futzu@users.noreply.github.com>
2019-12-23 16:47:49 -0500

    Validating PID and TID sooner

--- threefive/stream.py
+++ threefive/stream.py
@@ -4,6 +4,10 @@ from struct import unpack
 PACKET_SIZE=188
 SYNC_BYTE=b'\x47'
 
+def as_int(bits):
+    bs= f'0b{bits}'
+    return int(bs,2)
+
 class Stream:
     def __init__(self,tsfile=None,show_null=True):
         self.splices=[]
@@ -23,15 +27,15 @@ class Stream:
 
 
     def parse_tspacket(self,packet):
-        # sync,two_bytes, one_byte = 4 byte header
+        # sync,two_bytes, one_byte = 4 byte header:
         sync,two_bytes,one_byte, cue = unpack('>BHB184s', packet)
+        pid = two_bytes & 0x1fff
+        if self.PID:
+            if pid !=self.PID: return
         cue=cue[1:]
         if cue[0] !=0xfc : return
         if cue[13]==0:
             if not self.show_null: return 
-        pid = two_bytes & 0x1fff
-        if self.PID:
-            if pid !=self.PID: return
         tei = two_bytes >> 15
         pusi = two_bytes >> 14 & 0x1
         ts_priority = two_bytes >>13 & 0x1
@@ -40,8 +44,10 @@ class Stream:
         count = one_byte & 15
         try: tf=Splice(cue)
         except: return 
+        if not self.PID: 
+            self.PID=pid
+            print(f'\n\n[  SCTE 35 Stream found with Pid {hex(self.PID)}  ]')
         tf.show()
         self.splices.append(tf)
-        if not self.PID: self.PID=pid
         return
 
