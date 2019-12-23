6af7563d4d62d1b61e4b8453236b63ad3a330be7
CupCake of Doom <52701496+futzu@users.noreply.github.com>
2019-12-23 16:50:32 -0500

    removed tables import

--- threefive/util.py
+++ threefive/util.py
@@ -1,17 +1,13 @@
 import base64
 import bitstring
-try: import threefive.tables as tables
-except: import tables
 
-PACKET_SIZE=188
 
 def hex_decode(k):
     try: return bytearray.fromhex(hex(k)[2:]).decode()
     except: return k
 
 def kv_print(obj):
-    dotdot=' : '
-    for k,v in vars(obj).items(): print(f'{k}{dotdot}{v}')
+    print(f'\t{vars(obj)}')
  
 def mk_bits(s):
     if type(s) in [bitstring.BitStream,bitstring.ConstBitStream]: return s
@@ -24,6 +20,7 @@ def reserved(bb,bst):
 def time_90k(k):
     t= k/90000.0    
     return f'{t :.6f}'
+
 
 
 
