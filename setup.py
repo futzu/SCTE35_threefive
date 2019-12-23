464f6191cb10c9fc50061d95970c0c2ccbe6f8a9
CupCake of Doom <52701496+futzu@users.noreply.github.com>
2019-12-23 18:13:28 -0500

    1.1.81

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="threefive", 
-    version="1.1.79",
+    version="1.1.81",
     author="fu-corp",
     author_email="spam@futzu.com",
     description="SCTE 35 Message Decoder",
