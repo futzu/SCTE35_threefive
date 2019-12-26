ffaae49895da86e4d0e2a86fcb1271cada65043a
CupCake of Doom <52701496+futzu@users.noreply.github.com>
2019-12-25 21:08:09 -0500

    now using grab_bits

--- threefive/stream.py
+++ threefive/stream.py
@@ -1,12 +1,10 @@
 from .splice import Splice
 from struct import unpack
+from .util import grab_bits
+
 
 PACKET_SIZE=188
 SYNC_BYTE=b'\x47'
-
-def as_int(bits):
-    bs= f'0b{bits}'
-    return int(bs,2)
 
 class Stream:
     def __init__(self,tsfile=None,show_null=True):
@@ -18,31 +16,27 @@ class Stream:
     def parse_tsfile(self,tsfile):
         with open(tsfile,'rb') as tsdata:
             while tsdata:
-                sync_chk=tsdata.read(1)
-                if sync_chk==SYNC_BYTE:
-                    packet =sync_chk+ tsdata.read(PACKET_SIZE-1)
-                    if len(packet) == PACKET_SIZE:  self.parse_tspacket(packet)
-                    else: break
+                if tsdata.read(1)==SYNC_BYTE:
+                    packet =tsdata.read(PACKET_SIZE - 1)
+                    if not packet:  break
+                    self.parse_tspacket(packet)
                 else: return 
 
-
     def parse_tspacket(self,packet):
-        # sync,two_bytes, one_byte = 4 byte header:
-        sync,two_bytes,one_byte, cue = unpack('>BHB184s', packet)
-        pid = two_bytes & 0x1fff
+        two_bytes,one_byte,_, cue = unpack('>HBB183s', packet)
+        pid=grab_bits(two_bytes,12,13)
         if self.PID:
             if pid !=self.PID: return
-        cue=cue[1:]
         if cue[0] !=0xfc : return
-        if cue[13]==0:
+        if cue[13]==0: 
             if not self.show_null: return 
-        tei = two_bytes >> 15
-        pusi = two_bytes >> 14 & 0x1
-        ts_priority = two_bytes >>13 & 0x1
-        scramble = one_byte >>6
-        afc = (one_byte & 48) >> 4
-        count = one_byte & 15
-        try: tf=Splice(cue)
+        tei=grab_bits(two_bytes,15,1)
+        pusi=grab_bits(two_bytes,14,1)
+        ts_priority=grab_bits(two_bytes,13,1)
+        scramble=grab_bits(one_byte,7,2)
+        afc=grab_bits(one_byte,5,2)
+        count=grab_bits(one_byte,3,4)
+        try:tf=Splice(cue)
         except: return 
         if not self.PID: 
             self.PID=pid
