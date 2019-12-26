6d757717cb06d62d6d7335461a2bab5821db9d97
CupCake of Doom <52701496+futzu@users.noreply.github.com>
2019-12-26 17:32:49 -0500

    added btoi()

--- threefive/util.py
+++ threefive/util.py
@@ -1,9 +1,14 @@
 import base64
 import bitstring
 
+def btoi(bdata):
+    if type(bdata) == bytes: return int.from_bytes(bdata,byteorder='big')
+    else: return int.from_bytes(bytes(bdata),byteorder='big')
 
-def bit_slice(data,start_bit,bit_count):
-    return (data >> (start_bit+1-bit_count)) & ~(~0 << bit_count)
+def bitslice(data,bit_idx,num_bits):
+    if type(data) != int: 
+        data=btoi(data)
+    return (data >> (bit_idx+1-num_bits)) & ~(~0 << num_bits)
 
 
 def hex_decode(k):
@@ -24,6 +29,7 @@ def reserved(bb,bst):
 def time_90k(k):
     t= k/90000.0    
     return f'{t :.6f}'
+
 
 
 
