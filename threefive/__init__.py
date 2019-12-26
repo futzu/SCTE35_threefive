94e08486721dee63f8d8536fc264c03099436a21
CupCake of Doom <52701496+futzu@users.noreply.github.com>
2019-12-26 01:57:56 -0500

    Update __init__.py

--- threefive/__init__.py
+++ threefive/__init__.py
@@ -1,6 +1,8 @@
+from .decode import decode
 from .descriptors import *
 from .splice import Splice
 from .splice_commands import *
 from .splice_info_section import Splice_Info_Section
 from .stream import Stream
 from .util import *
+
