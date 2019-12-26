3885a7c40e9fd14b70549b0949fd85eafebe39fa
CupCake of Doom <52701496+futzu@users.noreply.github.com>
2019-12-26 17:35:54 -0500

    removed unpack calls in favor of bitslice

--- threefive/stream.py
+++ threefive/stream.py
@@ -1,14 +1,11 @@
 from .splice import Splice
-from struct import unpack
-from .util import bit_slice
-
+from .util import bitslice, btoi
 
 PACKET_SIZE=188
 SYNC_BYTE=b'\x47'
 
 
 class Stream:
-
     def __init__(self,tsfile=None,show_null=True):
         self.splices=[]
         self.PID=False
@@ -25,22 +22,24 @@ class Stream:
                 else: return 
 
     def parse_tspacket(self,packet):
-        two_bytes,one_byte,_, cue = unpack('>HBB183s', packet)
-        pid=bit_slice(two_bytes,12,13)
+        two_bytes=btoi(packet[:2])
+        one_byte=packet[2]
+        pid=bitslice(two_bytes,12,13)
         if self.PID and (pid !=self.PID): return
-        if cue[0] !=0xfc : return
+        if packet[4] !=0xfc :return
+        cue=packet[4:]
         try:tf=Splice(cue)
         except: return 
         if not self.PID: 
             self.PID=pid
             print(f'\n\n[  SCTE 35 Stream found with Pid {hex(self.PID)}  ]')
         if not self.show_null and (cue[13]==0) : return
-        tei=bit_slice(two_bytes,15,1)
-        pusi=bit_slice(two_bytes,14,1)
-        ts_priority=bit_slice(two_bytes,13,1)
-        scramble=bit_slice(one_byte,7,2)
-        afc=bit_slice(one_byte,5,2)
-        count=bit_slice(one_byte,3,4)
+        tei=bitslice(two_bytes,15,1)
+        pusi=bitslice(two_bytes,14,1)
+        ts_priority=bitslice(two_bytes,13,1)
+        scramble=bitslice(one_byte,7,2)
+        afc=bitslice(one_byte,5,2)
+        count=bitslice(one_byte,3,4)
         tf.show()
         self.splices.append(tf)
         return
