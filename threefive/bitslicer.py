42fda255f7a96fae35737295ecdcf70ad5202a19
CupCake of Doom <52701496+futzu@users.noreply.github.com>
2019-12-31 01:31:00 -0500

    raise TypeError('bites needs to be type bytes')

--- threefive/bitslicer.py
+++ threefive/bitslicer.py
@@ -1,13 +1,13 @@
 class BitSlicer:
-    
     def __init__(self,bites):
         '''
         From bytes to bits
         '''
+        if not isinstance(bites,bytes):
+            raise TypeError('bites needs to be type bytes')
         self.bit_idx=(len(bites)*8)
         self.bits=int.from_bytes(bites,byteorder='big')
-        
-        
+           
     def slice(self,num_bits):
         '''
         Starting at self.bit_idx of self.bits, slice off num_bits of bits.
@@ -16,13 +16,11 @@ class BitSlicer:
         self.bit_idx -=num_bits
         return bitslice
         
-        
     def hexed(self,num_bits):
         '''
         return the hex value of a bitslice
         '''
         return hex(self.slice(num_bits))
-     
         
     def boolean(self,num_bits=1):
         '''
