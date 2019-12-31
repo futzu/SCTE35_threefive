ea30971a93fb750cf476cfda8ff55df848097e16
CupCake of Doom <52701496+futzu@users.noreply.github.com>
2019-12-31 10:43:17 -0500

    added bitslicer9k

--- threefive/stream.py
+++ threefive/stream.py
@@ -1,6 +1,6 @@
 from .splice import Splice
 from .util import bitslice
-
+from bitslicer9k import BitSlicer9k
 
 PACKET_SIZE=188
 SYNC_BYTE=b'\x47'
@@ -25,24 +25,29 @@ class Stream:
 
     def parse_tspacket(self,packet):
         if packet[4] !=0xfc :return
-        two_bytes=packet[:2]
-        one_byte=packet[2]
-        pid=bitslice(two_bytes,12,13)
+        three_bytes=BitSlicer9k(packet[:2])
+        tei=three_bytes.boolean(1)
+        pusi=three_bytes.boolean(1)
+        ts_priority=three_bytes.boolean(1)
+        pid=three_bytes.slice(13)
         if self.PID and (pid !=self.PID): return
         cue=packet[4:]
         try:tf=Splice(cue)
         except: return 
         if not self.PID: 
-            self.PID=pid
-            print(f'\n\n[  SCTE 35 Stream found with Pid {hex(self.PID)}  ]')
+           self.PID=pid
+           print(f'\n\n[  SCTE 35 Stream found with Pid {hex(self.PID)}  ]')
         if not self.show_null and (cue[13]==0) : return
-        tei=bitslice(two_bytes,15,1)
-        pusi=bitslice(two_bytes,14,1)
-        ts_priority=bitslice(two_bytes,13,1)
+        '''
         scramble=bitslice(one_byte,7,2)
         afc=bitslice(one_byte,5,2)
         count=bitslice(one_byte,3,4)
+        '''
         tf.show()
         self.splices.append(tf)
         return
 
+    def show(self):
+        for s in self.splices:
+            print( f'\n[ Splice {self.splices.index(s)} ]')
+            s.show()
