4b2f000c115a4403a83bffdbb7a1eb55d9aa68f1
futzu <52701496+futzu@users.noreply.github.com>
2020-01-01 12:18:14 -0500

    removed util import

--- threefive/__init__.py
+++ threefive/__init__.py
@@ -1,9 +1,7 @@
 from .decode import decode
 from .descriptors import *
-from .splice import Splice
+from .splice import *
 from .splice_commands import *
 from .splice_info_section import Splice_Info_Section
 from .stream import Stream
-from .util import *
-
 
