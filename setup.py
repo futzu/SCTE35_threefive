0bbc925d8747b0b34c4049e8b8114f735a4abf11
futzu <52701496+futzu@users.noreply.github.com>
2020-01-01 12:29:03 -0500

    2.0.15

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="threefive", 
-    version="2.0.13",
+    version="2.0.15",
     author="fu-corp",
     author_email="spam@futzu.com",
     description="SCTE 35 Parser/Decoder",
