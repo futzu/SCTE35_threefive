c49c8fd1f830a30838d5b074e372e9d2ea22416e
futzu <52701496+futzu@users.noreply.github.com>
2020-02-03 01:54:53 -0500

    added parsevalues and novalues methods

--- threefive/splice_info_section.py
+++ threefive/splice_info_section.py
@@ -2,8 +2,34 @@ class Splice_Info_Section:
     """
     Table 5 - splice_info_section()
     """
+    def __init__(self, bs = None):
+        if not bs: self.novalues()
+        else: self.parsevalues(bs)
+ 
+    def novalues(self):
+        '''
+        create a splice_info_section instance
+        with all vars set to None
+        '''                 
+        self.table_id = None
+        self.section_syntax_indicator = None
+        self.private = None
+        self.reserved = None
+        self.section_length = None
+        self.protocol_version = None
+        self.encrypted_packet = None
+        self.encryption_algorithm =None
+        self.pts_adjustment = None
+        self.cw_index = None
+        self.tier = None
+        self.splice_command_length = None
+        self.splice_command_type = None
 
-    def __init__(self, bs):
+    def parsevalues(self, bs):
+        '''
+        create a splice_info_section instance
+        and set vars from bs
+        '''
         self.table_id = bs.ashex(8)
         self.section_syntax_indicator = bs.asflag(1)
         self.private = bs.asflag(1)
