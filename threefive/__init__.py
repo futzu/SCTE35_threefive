1482d1ab4dd3374dd328e59711a6e9f8094d4d50
futzu <52701496+futzu@users.noreply.github.com>
2020-02-16 02:49:27 -0500

    removing excess modules

--- threefive/__init__.py
+++ threefive/__init__.py
@@ -1,6 +1,3 @@
 from .decode import decode
-from .descriptors import *
-from .splice import *
-from .splice_commands import *
-from .splice_info_section import Splice_Info_Section
+from .splice import Splice
 from .stream import Stream
