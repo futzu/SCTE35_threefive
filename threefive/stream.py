8d805085c2801a81042e3eefea581c51f481b2d8
futzu <52701496+futzu@users.noreply.github.com>
2020-02-16 02:58:10 -0500

    performance tuned with cProfile

--- threefive/stream.py
+++ threefive/stream.py
@@ -7,10 +7,10 @@ class Stream:
     Parse mpegts files and streams for SCTE 35 packets
     '''
     PACKET_SIZE = 188
-    SYNC_BYTE = b'\x47'
+    SYNCBYTE = 0x47
     NON_PTS_STREAM_IDS = [188,190,191,240,241,242,248]
                     
-    def __init__(self,tsfile = None,tsstream = None,show_null = True):
+    def __init__(self,tsfile = None,tsstream = None,show_null = False):
         self.PID = False
         self.show_null = show_null
         self.tf = False
@@ -23,23 +23,27 @@ class Stream:
 
     def parse_tsdata(self,tsdata):
         while tsdata:
-            sb = tsdata.read(1) 
-            if sb == self.SYNC_BYTE: 
-                packet = tsdata.read(self.PACKET_SIZE - 1)
-                if packet: self.parse_tspacket(packet)
-                else: break
-            else: return 
-
+            tenpackets=tsdata.read(self.PACKET_SIZE *10)
+            if not tenpackets: break
+            while tenpackets:
+                p,tenpackets = tenpackets[:188], tenpackets[188:]
+                if p[0]== self.SYNCBYTE:
+                    self.parse_tspacket(p[1:])
+                    
     def parse_pusi(self, packet):
-        bitbin= BitBin(packet[3:]) 
+        bitbin= BitBin(packet) 
         if bitbin.asint(24) == 1 and bitbin.asint(8) not in self.NON_PTS_STREAM_IDS :
-            PES_packet_length = bitbin.asint(16)
+            #PES_packet_length = 
+            bitbin.asint(16)
             if bitbin.asint(2) == 2:
+                bitbin.asint(6)
+                '''
                 PES_scramble_control = bitbin.asint(2)
                 PES_priority = bitbin.asint(1)
                 data_align_ind = bitbin.asint(1)
                 copyright = bitbin.asint(1)
                 orig_or_copy = bitbin.asint(1)
+                '''
                 if bitbin.asint(2) == 2:
                     bitbin.asint(14)
                     if bitbin.asint(4) == 2:
@@ -52,23 +56,24 @@ class Stream:
                         fpts = f'PTS \033[92m{d:.3f}\033[0m ' 
                         print(f'\r{fpts}', end = "\r")
                                                                                             
-    def parse_tspacket(self,packet):
+    def parse_tspacket(self,packet):        
         two_bytes,one_byte = unpack('>HB', packet[:3])
         tei = two_bytes >> 15 
         pusi = two_bytes >> 14 & 0x1
         ts_priority = two_bytes >>13 & 0x1
         pid = two_bytes & 0x1fff
-        scramble = one_byte >>6
-        afc = (one_byte & 48) >> 4
-        count = one_byte & 15
-        if pusi: self.parse_pusi(packet)
+        #scramble = one_byte >>6
+        #afc = (one_byte & 48) >> 4
+        #count = one_byte & 15
+
+        if pusi: 
+            self.parse_pusi(packet[3:19])
         if packet[4] !=0xfc: return
-        cue = packet[4:]
         if pid  == 101: return     
         if self.PID and (pid != self.PID): return
         if not self.show_null:
             if packet[17] == 0: return
-        try: self.tf = Splice(cue)           
+        try: self.tf = Splice(packet[4:])           
         except: return
         print()
         self.tf.show()
