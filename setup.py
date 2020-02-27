06e651a727ec902deecca6475dc2f7f5f6e655b2
Adrian <52701496+futzu@users.noreply.github.com>
2020-02-27 04:52:00 -0500

    version bumped to 2.0.41

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="threefive",
-    version="2.0.39",
+    version="2.0.41",
     author="fu-corp",
     author_email="spam@futzu.com",
     description="SCTE 35 Parser/Decoder",
