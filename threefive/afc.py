014395a820b7ca1a421bae0ad1fd4ced5ed3cd05
Adrian <52701496+futzu@users.noreply.github.com>
2020-02-29 13:18:25 -0500

    added missing transport_private_data_flag

--- threefive/afc.py
+++ threefive/afc.py
@@ -13,15 +13,16 @@ def adaptation_fields(bitbin):
         random_access_indicator=bitbin.asflag(1)
         elementary_stream_priority_indicator=bitbin.asflag(1) 
         PCR_flag=bitbin.asflag(1)
-        OPCR_flag=bitbin.asflag(1)                                                                        
+        OPCR_flag=bitbin.asflag(1)                                                                   
         splicing_point_flag=bitbin.asflag(1) 
+        transport_private_data_flag=bitbin.asflag(1)
         adaptation_field_extension_flag=bitbin.asflag(1)
         
         if PCR_flag:
             program_clock_reference_base =bitbin.asint(33)    
             reserved=bitbin.forward(6)
             program_clock_reference_extension = bitbin.asint(9)  
-            print(f'PCR {program_clock_reference_base}')
+            #print(f'PCR {program_clock_reference_base}')
        
         if OPCR_flag:
             original_program_clock_reference_base = bitbin.as90k(33)
@@ -30,7 +31,7 @@ def adaptation_fields(bitbin):
             
         if splicing_point_flag:  
                     splice_countdown = bitbin.as90k(8)
-                    print(f'Splice Countdown: {splice_countdown}')
+                    #print(f'Splice Countdown: {splice_countdown}')
                     
         if transport_private_data_flag:
             tpdl = transport_private_data_length = bitbin.as90k(8)     
