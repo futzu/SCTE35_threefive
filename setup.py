91d08197430432d067248526a3e1233d4ee6e6f2
Adrian <52701496+futzu@users.noreply.github.com>
2020-05-02 15:51:56 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="threefive",
-    version="2.0.97",
+    version="2.0.99",
     author="fu-corp",
     author_email="spam@futzu.com",
     description="A SCTE 35 Decoder for Mpeg-TS video, and Base64 or Hex encoded messages", 
