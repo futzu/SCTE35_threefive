fc967a429471ec77191e77412795cf700ad272de
Adrian <52701496+futzu@users.noreply.github.com>
2020-05-05 01:50:58 -0400

    Update cli.py

--- examples/cli.py
+++ examples/cli.py
@@ -2,17 +2,13 @@
 
 import sys
 import threefive
-
-"""
-example command line tool.
-pass in a file name or message string to decode
-
-example:
-python cli.py '/DBIAAAAAAAA///wBQb+ek2ItgAyAhdDVUVJSAAAGH+fCAgAAAAALMvDRBEAAAIXQ1VFSUgAABl/nwgIAAAAACyk26AQAACZcuND'
-
-or
-python cli.py /path/to/mpeg.ts
-
-"""
-
-threefive.decode(sys.argv[1])
+ 
+def do():
+    try: 
+        threefive.decode(sys.argv[1])
+    except: 
+        # Handles piped in data
+        try: threefive.decode()
+        except: pass
+if __name__ == '__main__':
+    do()   
