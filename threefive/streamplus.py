8e1ec2f76bd76a06a24aee5db4f517cf009f27b4
Adrian <52701496+futzu@users.noreply.github.com>
2020-05-12 13:20:20 -0400

    removed struct.unpack

--- threefive/streamplus.py
+++ threefive/streamplus.py
@@ -1,8 +1,6 @@
 from .splice import Splice
 from bitn import BitBin
 from .stream import Stream
-from struct import unpack
-
 
 class StreamPlus(Stream):
     '''
@@ -12,7 +10,7 @@ class StreamPlus(Stream):
     NON_PTS_STREAM_IDS = [188, 190, 191, 240, 241, 242, 248]
 
     def __init__(self, tsdata, show_null = False):
-        self.PTS= False
+        self.PTS = False
         super().__init__(tsdata,show_null)
 
     def parse_pts(self,bitbin):
@@ -35,7 +33,7 @@ class StreamPlus(Stream):
         If the pusi data contains these markers,
         we can pull a PTS value..
         '''
-        if  packetdata[2] != 1: return False
+        if packetdata[2] != 1: return False
         if packetdata[3] in self.NON_PTS_STREAM_IDS: return False 
         if (packetdata[6] >> 6) != 2: return False
         if (packetdata[7] >> 6) != 2: return False
@@ -58,7 +56,7 @@ class StreamPlus(Stream):
         '''
         parse a mpegts packet for SCTE 35 and/or PTS
         '''
-        two_bytes = unpack('>H', packet[1:3])[0]
+        two_bytes = int.from_bytes(packet[1:3],byteorder='big')
         pid = two_bytes & 0x1fff
         pusi = two_bytes >> 14 & 0x1
         if pusi: self.parse_pusi(packet[4:20])
