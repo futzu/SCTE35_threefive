6210be7423e669dd4f6297a3e63cc79e9d9b6635
Adrian <52701496+futzu@users.noreply.github.com>
2020-05-14 02:24:10 -0400

    Update Time_Signal_Blackout_Override_Program_End.py

--- examples/Time_Signal_Blackout_Override_Program_End.py
+++ examples/Time_Signal_Blackout_Override_Program_End.py
@@ -7,9 +7,5 @@ from threefive import Splice
 Base64='/DBIAAAAAAAA///wBQb+ky44CwAyAhdDVUVJSAAACn+fCAgAAAAALKCh4xgAAAIXQ1VFSUgAAAl/nwgIAAAAACygoYoRAAC0IX6w'
 
 three5=Splice(Base64)
-print('Info Section')
-three5.show_info_section()
-print('Splice Command')
-three5.show_command()
-print('Splice Descriptors')
-three5.show_descriptors()
+three5.show()
+
