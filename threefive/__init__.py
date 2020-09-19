81cd76dfdcfe81a721ce5974942d542cdf304dc4
Adrian <52701496+futzu@users.noreply.github.com>
2020-09-19 14:00:40 -0400

    Update __init__.py

--- threefive/__init__.py
+++ threefive/__init__.py
@@ -2,7 +2,6 @@ from .decode import decode
 from .splice import Splice
 from .stream import Stream
 from .streamplus import StreamPlus
-from .splice_info_section import Splice_Info_Section
 
 def i2b(i,wide):
     return int.to_bytes(i,wide,byteorder='big')
