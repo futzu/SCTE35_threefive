17a1d9fb55d99f96e0c13a6bf496284c1c235b1c
a <a@fuhq>
2020-11-24 14:12:49 -0500

    tuple shuffle

--- threefive/version.py
+++ threefive/version.py
@@ -5,7 +5,7 @@ Used to set version in setup.py
 and as an easy way to check which
 version you have installed.
 """
-version_tuple = ("2.2.27","Nobody puts baby in the corner.".)
+version_tuple = ("2.2.27", "Two Two twenty-seven", "The 'Nobody puts baby in the corner' release")
 
 
 def version():
@@ -13,4 +13,4 @@ def version():
     version is set in the version_tuple
     to make it immutable.
     """
-    return version_tuple[0]
+    return version_tuple
