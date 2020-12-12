4982a441dbc1d7c187548d88a8945c242bea09da
a <a@fuhq>
2020-12-12 15:36:07 -0500

    updating documentation

--- threefive/stream.py
+++ threefive/stream.py
@@ -7,29 +7,30 @@ from .tools import CMD_TYPES, to_stderr
 
 
 def show_cue(cue):
-    '''
+    """
     default function call for
     Stream.decode,
     Stream.decode_program,
     and Stream.decode_proxy
     when a SCTE-35 packet is found.
-    '''
+    """
     cue.show()
 
 
 class Stream:
-    '''
+    """
     Stream class
     With MPEG-TS program awareness.
     Accurate pts for streams with
     more than one program containing
     SCTE-35 streams.
-    '''
+    """
+
     _CMD_TYPES = CMD_TYPES
     _PACKET_SIZE = 188
 
     def __init__(self, tsdata, show_null=False):
-        '''
+        """
         tsdata is an open file handle
         set show_null=True to include Splice Nulls
 
@@ -41,45 +42,22 @@ class Stream:
             strm = Stream(tsdata,show_null=True)
             strm.decode()
 
-        '''
+        """
         self._tsdata = tsdata
         if show_null:
             self._CMD_TYPES.append(0)
-        '''
-        set to hold SCTE35 pids
-        '''
+
         self._scte35_pids = set()
-        '''
-        map of pids to the program that holds them.
-        '''
         self._pid_prog = {}
-        '''
-        set of pids that hold program map tables.
-        '''
         self._pmt_pids = set()
-        '''
-        set of programs in the mpegts stream
-        '''
         self._programs = set()
-        '''
-        map of programs to the current pts
-        '''
         self._prog_pts = {}
-        '''
-        flag to trigger displaying the programs and streams
-        '''
-        self.info = False
-        '''
-        if set, only pids assigned to that program are parsed.
-        '''
-        self.the_program = False
-        '''
-        used to assemble multi-packet SCTE35 cues
-        '''
+        self.info = None
+        self.the_program = None
         self.cue = None
 
     def decode(self, func=show_cue):
-        '''
+        """
         Stream.decode reads self.tsdata to find SCTE35 packets.
 
         func is the function called to handle each
@@ -96,7 +74,7 @@ class Stream:
             def myfunc(cue):
                 # access cue data using dot notation
                 print(f'Table Id: {cue.info_section.table_id}')
-        '''
+        """
         self._find_start()
         for pkt in iter(partial(self._tsdata.read, self._PACKET_SIZE), b""):
             cue = self._parser(pkt)
@@ -104,20 +82,20 @@ class Stream:
                 func(cue)
 
     def decode_program(self, the_program, func=show_cue):
-        '''
+        """
         Stream.decode_program works just like Stream.decode
         except the argument, the_program limits SCTE35 parsing
         to that MPEGTS program.
-        '''
+        """
         self.the_program = the_program
         self.decode(func)
 
     def decode_proxy(self, func=show_cue):
-        '''
+        """
         Stream.decode_proxy works just like Stream.decode
         except that all ts packets are written to stdout
         for piping into another program like mplayer.
-        '''
+        """
         self._find_start()
         for pkt in iter(partial(self._tsdata.read, self._PACKET_SIZE), b""):
             sys.stdout.buffer.write(pkt)
@@ -126,23 +104,16 @@ class Stream:
                 func(cue)
 
     def show(self):
-        '''
+        """
         displays all programs and stream mappings
-
-        Program: 1030
-           1031: [0x1b] Video
-           1032: [0x3] ISO/IEC 11172 Audio
-           1034: [0x6] ITU-T Rec. H.222.0
-           1035: [0x86] SCTE 35
-
-        '''
+        """
         self.info = True
         self.decode()
 
     def _find_start(self):
-        '''
+        """
         handles partial packets
-        '''
+        """
         sync_byte = b"G"
         while self._tsdata:
             if self._tsdata.read(1) == sync_byte:
@@ -150,10 +121,10 @@ class Stream:
                     return
 
     def _mk_packet_data(self, pid):
-        '''
+        """
         creates packet_data dict
         to pass to a threefive.Cue instance
-        '''
+        """
         packet_data = {}
         packet_data["pid"] = pid
         prgm = self._pid_prog[pid]
@@ -163,10 +134,10 @@ class Stream:
         return packet_data
 
     def _parser(self, pkt):
-        '''
+        """
         parse pid from pkt and
         route it appropriately
-        '''
+        """
         pid = self._parse_pid(pkt[1], pkt[2])
         if pid == 0:
             self._program_association_table(pkt)
@@ -174,7 +145,6 @@ class Stream:
         if pid in self._pmt_pids:
             self._program_map_section(pkt)
             return None
-            # This return makes Stream.show() fast
         if self.info:
             return None
         if pid in self._scte35_pids:
@@ -186,16 +156,16 @@ class Stream:
 
     @staticmethod
     def _parse_pid(byte1, byte2):
-        '''
+        """
         parse pid from packet
-        '''
+        """
         return (byte1 & 31) << 8 | byte2
 
     def _parse_program_streams(self, slib, bitbin, program_number):
-        '''
+        """
         parse the elementary streams
         from a program
-        '''
+        """
         if program_number not in self._programs:
             self._programs.add(program_number)
             if self.info:
@@ -215,10 +185,10 @@ class Stream:
                 sys.exit()
 
     def _parse_pts(self, pkt, pid):
-        '''
+        """
         parse pts and store by program key
         in the dict Stream._prog_pts
-        '''
+        """
         pts = ((pkt[13] >> 1) & 7) << 30
         pts |= ((pkt[14] << 7) | (pkt[15] >> 1)) << 15
         pts |= (pkt[16] << 7) | (pkt[17] >> 1)
@@ -227,9 +197,9 @@ class Stream:
         self._prog_pts[ppp] = pts
 
     def _parse_pusi(self, pkt, pid):
-        '''
+        """
         used to determine if pts data is available.
-        '''
+        """
         if pkt[6] & 1:
             if (pkt[10] >> 6) & 2:
                 if (pkt[11] >> 6) & 2:
@@ -237,9 +207,9 @@ class Stream:
                         self._parse_pts(pkt, pid)
 
     def _parse_scte35(self, pkt, pid):
-        '''
+        """
         parse a scte35 cue from one or more packets
-        '''
+        """
         if not self.cue:
             packet_data = self._mk_packet_data(pid)
             if pkt[18] in self._CMD_TYPES:
@@ -259,9 +229,9 @@ class Stream:
 
     @staticmethod
     def _parse_stream_type(bitbin):
-        '''
+        """
         extract stream pid and type
-        '''
+        """
         stream_type = bitbin.ashex(8)  # 8
         bitbin.forward(3)  # 11
         el_pid = bitbin.asint(13)  # 24
@@ -272,10 +242,10 @@ class Stream:
         return minus, [stream_type, el_pid]
 
     def _program_association_table(self, pkt):
-        '''
+        """
         parse program association table ( pid 0 )
         to program to program table pid mappings.
-        '''
+        """
         sectionlen = (pkt[6] & 15 << 8) | pkt[7]
         pkt = pkt[13 : (sectionlen + 5)]
         bitbin = BitBin(pkt)
@@ -292,9 +262,9 @@ class Stream:
         bitbin.forward(32)
 
     def _program_map_section(self, pkt):
-        '''
+        """
         parse program maps for streams
-        '''
+        """
         # table_id = pkt[5]
         sectioninfolen = (pkt[6] & 15 << 8) | pkt[7]
         slib = sectioninfolen << 3
@@ -316,9 +286,9 @@ class Stream:
 
     @staticmethod
     def _show_program_stream(pid, stream_type):
-        '''
+        """
         print program -> stream mappings
-        '''
+        """
         streaminfo = f"[{stream_type}] Reserved or Private"
         if stream_type in stream_type_map.keys():
             streaminfo = f"[{stream_type}] {stream_type_map[stream_type]}"
