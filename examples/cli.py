16bb81d01f6d8de7bd9cab8c48e5fa0bbce58e37
Adrian <52701496+futzu@users.noreply.github.com>
2020-12-23 23:32:11 -0500

    Now handles multiple inputs of files and strings

--- examples/cli.py
+++ examples/cli.py
@@ -1,16 +1,43 @@
 #!/usr/bin/env python3
+"""
+
+Usage One:
+----------
+
+    Pass one or more files,
+    Base64 encoded strings,
+    or Hex encoded strings
+    on the command line.
+
+        pypy3 cli.py ../u.ts \
+                /usr/a/35.ts \
+                "0xFC301100000000000000FFFFFF0000004F253396" \
+                "/DAvAAAAAAAA///wFAVIAACPf+/+c2nALv4AUsz1AAAAAAAKAAhDVUVJAAABNWLbowo=" \
+                /usr/a/cue.ts
+
+
+Usage Two:
+----------
+
+    Pipe data into cli.py
+
+        cat video.ts | pypy3 cli.py
+
+"""
 
 import sys
 import threefive
 
 
 def do():
-    try:
-        threefive.decode(sys.argv[1])
-    except:
-        # Handles piped in data
+    if not sys.argv[1:]:
+        threefive.decode()
+    else:
         try:
-            threefive.decode()
+            args = sys.argv[1:]
+            for arg in args:
+                print(f"Next is {arg}\n\n")
+                threefive.decode(arg)
         except:
             pass
 
