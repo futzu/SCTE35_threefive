151cbc222bf3e958f9e09b59c1ea247f340f53fe
Adrian <52701496+futzu@users.noreply.github.com>
2021-01-08 13:13:11 -0500

    removed encoding method

--- threefive/section.py
+++ threefive/section.py
@@ -3,8 +3,7 @@ section.py
 
 SCTE35 Splice Info Section
 """
-from base64 import b64encode
-from threefive.tools import i2b, ifb, to_stderr
+from threefive.tools import ifb
 
 
 class SpliceInfoSection:
@@ -60,41 +59,3 @@ class SpliceInfoSection:
         self.splice_command_length = (bites[11] & 15) << 8 | bites[12]
         self.splice_command_type = bites[13]
         self.descriptor_loop_length = 0
-
-    def encode(self):
-        """
-        encode the SCTE35 splice info section
-        """
-        first_byte = int(self.table_id, 16)
-        bencoded = i2b(first_byte, 1)
-
-        two_bytes = 0
-        if self.section_syntax_indicator:
-            two_bytes = 1 << 15
-        if self.private:
-            two_bytes += self.private << 14
-        two_bytes += int(self.reserved, 16) << 12
-        two_bytes += self.section_length
-        bencoded += i2b(two_bytes, 2)
-
-        proto_byte = self.protocol_version
-        bencoded += i2b(proto_byte, 1)
-
-        five_bytes = 0
-        if self.encrypted_packet:
-            five_bytes = 1 << 39
-        five_bytes += self.encryption_algorithm << 33
-        five_bytes += int(self.pts_adjustment * 90000)
-        bencoded += i2b(five_bytes, 5)
-
-        cw_byte = int(self.cw_index, 16)
-        bencoded += i2b(cw_byte, 1)
-
-        three_bytes = int(self.tier, 16) << 12
-        three_bytes += self.splice_command_length
-        bencoded += i2b(three_bytes, 3)
-
-        cmd_byte = self.splice_command_type
-        bencoded += i2b(cmd_byte, 1)
-        to_stderr(bencoded)
-        to_stderr(b64encode(bencoded))
