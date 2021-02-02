956a3b825680e36b741932343b68fd65cd75768a
Richard van Dijk <richard@itnm.nl>
2021-02-02 19:21:49 +0100

    fix 'Address already in use' in mcastc.py example
    SO_REUSEADDR only works when set before bind().
    The error occurred when running multiple multicast clients on the same UDP port.

--- examples/multicast/mcastc.py
+++ examples/multicast/mcastc.py
@@ -48,13 +48,13 @@ def mk_sock(mcast_host, mcast_ip, mcast_port):
     multicast socket setup
     """
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    sock.bind((mcast_host, mcast_port))
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.setsockopt(
         socket.IPPROTO_IP,
         socket.IP_ADD_MEMBERSHIP,
         socket.inet_aton(mcast_ip) + socket.inet_aton(mcast_host),
     )
+    sock.bind((mcast_host, mcast_port))
     return sock
 
 
