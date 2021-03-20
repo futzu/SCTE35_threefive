309fe8e26cd6b78964f4815d1ce658f4768aa724
Sweetness <a@futzu.com>
2021-03-19 22:56:49 -0400

    black

--- examples/stream/Decode_Over_Http.py
+++ examples/stream/Decode_Over_Http.py
@@ -6,7 +6,7 @@ without downloading first.
 import urllib3
 from threefive import Stream
 
-video = 'https://futzu.com/xaa.ts'
+video = "https://futzu.com/xaa.ts"
 
 http = urllib3.PoolManager()
 req = http.request("GET", video, preload_content=False)
