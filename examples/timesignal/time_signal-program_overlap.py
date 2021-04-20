2bc418c3fe3da728cbf22ceb3c097688230776a2
Adrian <52701496+futzu@users.noreply.github.com>
2021-04-20 09:44:24 -0400

    Update and rename Time_Signal-Program_Overlap.py to time_signal-program_overlap.py

---
+++ examples/timesignal/time_signal-program_overlap.py
@@ -0,0 +1,13 @@
+"""
+Example from the specification
+
+14.5. Time_Signal–Program_Overlap_Start
+
+"""
+
+
+from threefive import decode
+
+hexed = "0xFC302F000000000000FFFFF00506FEAEBFFF640019021743554549480000087F9F0808000000002CA56CF5170000951DB0A8"
+
+decode(hexed)
