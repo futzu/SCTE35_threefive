7aa634edaccfdd56f794fff4f476f18544a40296
a <a@futzu.com>
2021-06-18 06:40:12 -0400

    Partial Segmentation Descriptor

--- go/descriptors.go
+++ go/descriptors.go
@@ -4,18 +4,29 @@ import "github.com/futzu/bitter"
 
 // Splice descriptor
 type SpDscptr struct {
-	DescriptorType uint64
-	DescriptorLen  uint64
+	Tag    uint64
+	Length uint64
+	Name   string
 	// identiﬁer 32 uimsbf == 0x43554549 (ASCII “CUEI”)
-	Identifier      string
-	Name            string
+	ID string
+
 	ProviderAvailID uint64
-	PreRoll         uint64   `json:",omitempty"`
-	DTMFCount       uint64   `json:",omitempty"`
-	DTMFChars       []string `json:",omitempty"`
-	TAISeconds      uint64   `json:",omitempty"`
-	TAINano         uint64   `json:",omitempty"`
-	UTCOffset       uint64   `json:",omitempty"`
+
+	PreRoll                          uint64   `json:",omitempty"`
+	DTMFCount                        uint64   `json:",omitempty"`
+	DTMFChars                        []string `json:",omitempty"`
+	TAISeconds                       uint64   `json:",omitempty"`
+	TAINano                          uint64   `json:",omitempty"`
+	UTCOffset                        uint64   `json:",omitempty"`
+	SegmentationEventID              uint64   `json:",omitempty"`
+	SegmentationEventCancelIndicator bool     `json:",omitempty"`
+	ProgramSegmentationFlag          bool     `json:",omitempty"`
+	SegmentationDurationFlag         bool     `json:",omitempty"`
+	DeliveryNotRestrictedFlag        bool     `json:",omitempty"`
+	WebDeliveryAllowedFlag           bool     `json:",omitempty"`
+	NoRegionalBlackoutFlag           bool     `json:",omitempty"`
+	ArchiveAllowedFlag               bool     `json:",omitempty"`
+	//  DeviceRestrictions = table20[bitn.AsUInt64(2)]
 	/**
 	  SegmentationEventId
 	  segmentation_event_cancel_indicator
@@ -43,21 +54,23 @@ type SpDscptr struct {
 
 // MetaData for splice descriptors
 func (dscptr *SpDscptr) MetaData(bitn *bitter.Bitn) {
-	dscptr.DescriptorType = bitn.AsUInt64(8)
-	dscptr.DescriptorLen = bitn.AsUInt64(8)
-	dscptr.Identifier = bitn.AsHex(32)
+	dscptr.Tag = bitn.AsUInt64(8)
+	dscptr.Length = bitn.AsUInt64(8)
+	dscptr.ID = bitn.AsHex(32)
 }
 
 // Decode splice descriptor values
 func (dscptr *SpDscptr) Decode(bitn *bitter.Bitn) {
-	ddt := dscptr.DescriptorType
+	ddt := dscptr.Tag
 	if ddt == 0 {
 		dscptr.AvailDscptr(bitn)
 	}
 	if ddt == 1 {
 		dscptr.DTMFDscptr(bitn)
 	}
-
+	if ddt == 2 {
+		dscptr.SegmentDscptr(bitn)
+	}
 	if ddt == 3 {
 		dscptr.TimeDscptr(bitn)
 	}
@@ -90,3 +103,36 @@ func (dscptr *SpDscptr) TimeDscptr(bitn *bitter.Bitn) {
 	dscptr.TAINano = bitn.AsUInt64(32)
 	dscptr.UTCOffset = bitn.AsUInt64(16)
 }
+
+// SegmentDscptr Segmentation Descriptor
+func (dscptr *SpDscptr) SegmentDscptr(bitn *bitter.Bitn) {
+	dscptr.Name = "Segmentation Descriptor"
+	dscptr.SegmentationEventID = bitn.AsUInt64(32)
+	dscptr.SegmentationEventCancelIndicator = bitn.AsBool()
+	bitn.Forward(7)
+	if !(dscptr.SegmentationEventCancelIndicator) {
+		dscptr.DecodeSegFlags(bitn)
+		// if !(dscptr.ProgramSegmentationFlag {
+		//       dscptr.DecodeSegCmpnts(bitn)
+	}
+	//  dscptr.DecodeSeg(bitn)
+}
+
+func (dscptr *SpDscptr) DecodeSegFlags(bitn *bitter.Bitn) {
+	dscptr.ProgramSegmentationFlag = bitn.AsBool()
+	dscptr.SegmentationDurationFlag = bitn.AsBool()
+	dscptr.DeliveryNotRestrictedFlag = bitn.AsBool()
+	if !dscptr.DeliveryNotRestrictedFlag {
+		dscptr.WebDeliveryAllowedFlag = bitn.AsBool()
+		dscptr.NoRegionalBlackoutFlag = bitn.AsBool()
+		dscptr.ArchiveAllowedFlag = bitn.AsBool()
+		// dscptr.DeviceRestrictions = table20[bitn.AsUInt64(2)]
+		return
+	}
+	bitn.Forward(5)
+}
+
+//func (dscptr *SpDscptr) DecodeSegCmpnts((bitn *bitter.Bitn) {
+//    ccount = bitn.AsUint64(8)
+
+//}
