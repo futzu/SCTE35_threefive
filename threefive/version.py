e197dfea323dacb116fe0b9da1c1618bc6237abb
Adrian <52701496+futzu@users.noreply.github.com>
2021-08-25 00:59:48 -0400

    Update version.py

--- threefive/version.py
+++ threefive/version.py
@@ -10,8 +10,8 @@ version you have installed.
 """
 
 MAJOR = 2
-MINOR = 2
-MAINTAINENCE = 99
+MINOR = 3
+MAINTAINENCE = 00
 
 
 def version():
@@ -22,4 +22,9 @@ def version():
 
 
 def version_number():
+    """
+    version_number returns version as an int.
+    if version() returns 2.3.01
+    version_number will return 2301
+    """
     return (MAJOR * 1000) + (MINOR * 100) + MAINTAINENCE
