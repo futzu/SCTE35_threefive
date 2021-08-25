e214692f65a325e10e854694eebc643a12478b87
a <a@futzu.com>
2021-08-25 01:28:14 -0400

    ....

--- threefive/smoketest.py
+++ threefive/smoketest.py
@@ -4,6 +4,7 @@ smoke_test.py
 
 from .decode import decode
 
+# The format for tests is a dict of { "test_name" : value to pass to threefive.decode}
 six = {
     "Base64": "/DAvAAAAAAAA///wBQb+dGKQoAAZAhdDVUVJSAAAjn+fCAgAAAAALKChijUCAKnMZ1g=",
     "Bytes": b"\xfc0\x11\x00\x00\x00\x00\x00\x00\x00\xff\xff\xff\x00\x00\x00O%3\x96",
@@ -11,6 +12,7 @@ six = {
     "Hex Literal": 0xFC301100000000000000FFFFFF0000004F253396,
     "Integer": 1439737590925997869941740173214217318917816529814,
     "HTTP/HTTPS Streams": "https://futzu.com/xaa.ts",
+    # "Bad" tests are expected to fail.
     "Bad Base64 ": "/DAvAf45AA",
     "Bad File": "/you/me/fake.file",
     "Bad Integer": -0.345,
@@ -18,9 +20,10 @@ six = {
 }
 
 
-def _decode_test(test_data):
+def _decode_test(test_name, test_data):
     passed = "✔"
     failed = "✘"
+    print(f"testing {test_name}\n Data: {test_data}\n")
     try:
         decode(test_data)
         return passed
@@ -34,7 +37,7 @@ def smoke_test(tests=None):
     """
     if not tests:
         tests = six
-    results = {k: _decode_test(v) for k, v in tests.items()}
+    results = {k: _decode_test(k, v) for k, v in tests.items()}
     print("Smoke Test\n")
     {print(f"{k}  {v}") for k, v in results.items()}
 
