9af3d499d00a9d14e34aaa4d37961aced0bf8b94
a <a@futzu.com>
2021-09-25 01:31:51 -0400

    Adding methods to SCTE35Base for encoding

--- threefive/base.py
+++ threefive/base.py
@@ -2,6 +2,8 @@
 threefive.base contains
 the class SCTE35Base.
 """
+import json
+from .bitn import NBin
 
 
 class SCTE35Base:
@@ -12,6 +14,12 @@ class SCTE35Base:
 
     def __repr__(self):
         return str(self.get())
+
+    @staticmethod
+    def _chk_nbin(nbin):
+        if not nbin:
+            nbin = NBin()
+        return nbin
 
     def get(self):
         """
@@ -36,3 +44,28 @@ class SCTE35Base:
             return val
 
         return {k: b2l(v) for k, v in vars(self).items() if v is not None}
+
+    def load(self, stuff):
+        """
+        load is used to load
+        data from a dict or json string
+        """
+        if isinstance(stuff, str):
+            stuff = json.loads(stuff)
+        if isinstance(stuff, dict):
+            self.__dict__.update(stuff)
+
+    def _chk_var(self, var_type, nbin_method, var_name, bit_count):
+        """
+        _chk_var is used to check var values and types before encoding
+        """
+        var_value = self.__dict__[var_name]
+        if var_value is None:
+            err_mesg = (
+                f"\033[7m{var_name} is not set, it should be type {var_type}\033[27m"
+            )
+            raise ValueError(err_mesg)
+        if not isinstance(var_value, var_type):
+            err_mesg = f' \033[7m{var_name} is "{var_value}", it should be type {var_type}\033[27m '
+            raise ValueError(err_mesg)
+        nbin_method(var_value, bit_count)
