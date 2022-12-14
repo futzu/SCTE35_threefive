1b390689ee64e4b20080660ad6ca8a873f2559c0
Adrian <52701496+futzu@users.noreply.github.com>
2022-12-14 23:02:24 +0000

    going for style points

--- threefive/crc.py
+++ threefive/crc.py
@@ -8,27 +8,29 @@ INIT_VALUE = 0xFFFFFFFF
 SIZE_BITS = 32
 
 
-def _bytecrc(crc, poly, num):
-    mask = 1 << (num - 1)
+def _bytecrc(crc, poly):
+    mask = 1 << ( SIZE_BITS- 1)   
     i = 8
     while i:
         crc = (crc << 1, crc << 1 ^ poly)[crc & mask != 0]
         i -= 1
-    mask = (1 << num) - 1
-    crc = crc & mask
-    return crc
+    return crc & INIT_VALUE
 
 
 def _mk_table():
     mask = (1 << SIZE_BITS) - 1
     poly = POLY & mask
-    return [_bytecrc(i << (SIZE_BITS - 8), poly, SIZE_BITS) for i in range(256)]
+    return [_bytecrc(i << (SIZE_BITS - 8), poly) for i in range(256)]
 
 
 def crc32(data):
+    """
+    generate a 32 bit crc
+    """
     table = _mk_table()
     emvee = memoryview(data)
-    crc = INIT_VALUE & 0xFFFFFFFF
+    crc = INIT_VALUE 
     for bite in emvee.tobytes():
         crc = table[bite ^ ((crc >> 24) & 0xFF)] ^ ((crc << 8) & 0xFFFFFF00)
     return crc
+
