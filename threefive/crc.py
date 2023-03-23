9cdc0f5bfec1a47b0d991f2fe5953a27be1e1159
Adrian <52701496+futzu@users.noreply.github.com>
2023-03-23 00:05:07 +0000

    I was told to use more CONSTANTS

--- threefive/crc.py
+++ threefive/crc.py
@@ -4,23 +4,28 @@ crc.py  crc32 function for encoding.
 
 POLY = 0x104C11DB7
 INIT_VALUE = 0xFFFFFFFF
-FOUR_BYTES = 0x20
-THREE_BYTES = 0x18
+ZERO = 0x0
+ONE = 0x1
+EIGHT = 0x8
+TWENTY_FOUR = 0x18
+THIRTY_TWO = 0x20
+TWO_FIFTY_FIVE = 0xFF
+TWO_FIFTY_SIX = 0x100
 
 
 def _bytecrc(crc, poly):
-    mask = 1 << (FOUR_BYTES - 1)
-    i = 8
+    mask = ONE << (THIRTY_TWO - ONE)
+    i = EIGHT
     while i:
-        crc = (crc << 1, crc << 1 ^ poly)[crc & mask != 0]
-        i -= 1
+        crc = (crc << ONE, crc << ONE ^ poly)[crc & mask != ZERO]
+        i -= ONE
     return crc & INIT_VALUE
 
 
 def _mk_table():
-    mask = (1 << FOUR_BYTES) - 1
+    mask = (ONE << THIRTY_TWO) - ONE
     poly = POLY & mask
-    return [_bytecrc((i << THREE_BYTES), poly) for i in range(256)]
+    return [_bytecrc((i << TWENTY_FOUR), poly) for i in range(TWO_FIFTY_SIX)]
 
 
 def crc32(data):
@@ -30,5 +35,7 @@ def crc32(data):
     table = _mk_table()
     crc = INIT_VALUE
     for bite in data:
-        crc = table[bite ^ ((crc >> THREE_BYTES) & 0xFF)] ^ ((crc << 8) & 0xFFFFFF00)
+        crc = table[bite ^ ((crc >> TWENTY_FOUR) & TWO_FIFTY_FIVE)] ^ (
+            (crc << EIGHT) & (INIT_VALUE - TWO_FIFTY_FIVE)
+        )
     return crc
