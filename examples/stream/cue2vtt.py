59a8d847b8423714b93ca155c29af953ff275edd
Adrian <52701496+futzu@users.noreply.github.com>
2023-07-01 21:40:44 +0000

    Update cue2vtt.py

--- examples/stream/cue2vtt.py
+++ examples/stream/cue2vtt.py
@@ -10,7 +10,6 @@ pypy3 cue2vtt.py thevideo.ts | mplayer thevideo.ts -sub -
 """
 
 import sys
-
 from threefive import Stream
 
 
@@ -21,25 +20,38 @@ def ts_to_vtt(timestamp):
     hours, seconds = divmod(timestamp, 3600)
     mins, seconds = divmod(seconds, 60)
     seconds = round(seconds, 3)
-    return f" {int(hours):02}:{int(mins):02}:{seconds:02}"
+    return f"{int(hours):02}:{int(mins):02}:{seconds:02}"
 
 
 def scte35_to_vtt(cue):
     """
     scte35_to_vtt prints splice insert cue out and cue in via webvtt
     """
-
-    ts = 0
-    if cue.packet_data.pcr is not None:
-        ts = cue.packet_data.pcr
+    start =0
+    end = 0
+    duration = None
+    pts_time = cue.packet_data.pts
+    start = cue.packet_data.pts
+    if "pts_time" in cue.command.get():
+        pts_time = cue.command.pts_time
+    if "break_duration" in cue.command.get():
+        duration = cue.command.break_duration
+        end =  start + duration -1
     else:
-        if cue.packet_data.pts is not None:
-            ts = cue.packet_data.pts
-    start = ts
-    end = start + 5
-    print(f"{ts_to_vtt(start)} --> {ts_to_vtt(end)} align:left")
-    for k, v in cue.command.get().items():
-        print(k, " : ", v)
+        for d in cue.descriptors:
+            if "segmentation_duration" in d.get():
+                duration = d.segmentation_duration
+                end = start + duration -1
+    if end == 0:
+        end = start+4
+            
+    print(f"{ts_to_vtt(start)} --> {ts_to_vtt(end)} ")
+
+    print(f"Cmd: {cue.command.name}    ")
+    if pts_time:
+        print(f"PTS:  {pts_time}  ")
+    if duration:
+        print(f"Duration:  {round(duration,3)} " )
     print()
 
 
@@ -48,3 +60,4 @@ if __name__ == "__main__":
     print("WEBVTT\n\n\n")
     strm = Stream(arg)
     strm.decode(func=scte35_to_vtt)
+
