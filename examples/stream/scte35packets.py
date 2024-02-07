fb683f13e11c196ebd190c122ddec126f6933c4e
Adrian <52701496+futzu@users.noreply.github.com>
2024-02-06 22:38:41 -0500

    show raw SCTE-35 packets in a file or stream.

---
+++ examples/stream/scte35packets.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""
+scte35packets.py 
+
+    print out the raw SCTE-35 packets in a stream or file.
+
+
+Use like:
+
+    a@slow:~$ python3 scte35packets.py https://futzu.com/xaa.ts
+
+"""
+
+
+import sys
+from threefive import Stream
+
+
+
+class SupaStream(Stream):
+
+    def _parse_scte35(self,pkt,pid):
+        print(pkt)
+        super()._parse_scte35(pkt,pid)
+
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    for arg in args:
+        supa = SupaStream(arg)
+        supa.decode()
