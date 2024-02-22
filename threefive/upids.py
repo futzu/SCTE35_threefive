c0c2b8fa7005a5d38cfdf75e7725186810e5aa19
Adrian of Doom <52701496+futzu@users.noreply.github.com>
2024-02-21 19:54:18 -0500

    classy upids; cyclomatic complexity score : 1.689...seriously.

--- threefive/upids.py
+++ threefive/upids.py
@@ -3,32 +3,94 @@ threefive/upids.py
 
 threefve.upids
 
+classy Upids
+
+cyclomatic complexity 1.689
+
+
 """
-charset = "ascii"
+
+charset = "ascii"  # this isn't a constant pylint.
+
 """
 set charset to None to return raw bytes
 """
-charset = "ascii"
 
 
-class UpidDecoder:
+class Upid:
     """
-    UpidDecoder for use by the
-    SegmentationDescriptor class.
+    Upid base class handles URI UPIDS
     """
 
-    def __init__(self, bitbin, upid_type, upid_length):
+    def __init__(self, bitbin=None, upid_type=0, upid_length=0):
         self.bitbin = bitbin
         self.upid_type = upid_type
+        self.upid_name = upid_map[upid_type][0]
         self.upid_length = upid_length
         self.bit_length = upid_length << 3
 
-    def _decode_air_id(self):
-        return self.bitbin.as_hex(self.bit_length)
+    def decode(self):
+        """
+        decode Upid
+        """
+        return self.upid_name, self.bitbin.as_charset(self.bit_length, charset)
 
-    def _decode_atsc(self):
+    def encode(self, nbin, seg_upid):
+        """
+        encode Upid
+        """
+        if seg_upid:
+            seg_upid = seg_upid.encode("utf8")
+            nbin.add_bites(seg_upid)
+
+
+class NoUpid(Upid):
+    """
+    NoUpid class
+    """
+
+    def decode(self):
+        """
+        decode for no upid
+        """
+        return self.upid_name, None
+
+    def encode(self, nbin, seg_upid):
+        """
+        encode for no upid
+        """
+        nbin.forward(0)
+
+
+class AirId(Upid):
+    """
+    Air Id Upid
+    """
+
+    def decode(self):
+        """
+        decode AirId
+        """
+        return self.upid_name, self.bitbin.as_hex(self.bit_length)
+
+    def encode(self, nbin, seg_upid):
+        """
+        encode AirId
+        """
+        nbin.add_hex(seg_upid, (self.upid_length << 3))
+
+
+class Atsc(Upid):
+    """
+    ATSC Upid
+    """
+
+    def decode(self):
+        """
+        decode Atsc Upid
+        """
         cont_size = self.bit_length - 32
-        return {
+        return self.upid_name, {
             "TSID": self.bitbin.as_int(16),
             "reserved": self.bitbin.as_int(2),
             "end_of_day": self.bitbin.as_int(5),
@@ -36,7 +98,26 @@ class UpidDecoder:
             "content_id": self.bitbin.as_charset(cont_size, charset),
         }
 
-    def _decode_eidr(self):
+    def encode(self, nbin, seg_upid):
+        """
+        encode Atsc
+        """
+        nbin.add_int(seg_upid["TSID"], 16)
+        nbin.add_int(seg_upid["reserved"], 2)
+        nbin.add_int(seg_upid["end_of_day"], 5)
+        nbin.add_int(seg_upid["unique_for"], 9)
+        nbin.add_bites(seg_upid["content_id"].encode("utf-8"))
+
+
+class Eidr(Upid):
+    """
+    Eidr Upid
+    """
+
+    def decode(self):
+        """
+        decode Eidr Upid
+        """
         if self.upid_length != 12:
             return f"upid_length is {self.upid_length} should be 12 bytes."
         pre = self.bitbin.as_int(16)
@@ -45,12 +126,44 @@ class UpidDecoder:
         while bit_count:
             bit_count -= 16
             post.append(self.bitbin.as_hex(16)[2:])
-        return f"10.{pre}/{'-'.join(post)}"
+        return self.upid_name, f"10.{pre}/{'-'.join(post)}"
 
-    def _decode_isan(self):
-        return self.bitbin.as_hex(self.bit_length)
+    def encode(self, nbin, seg_upid):
+        """
+        encode Eidr Upid
+        """
+        pre, post = seg_upid[3:].split("/", 1)
+        nbin.add_int(int(pre), 16)
+        nbin.add_hex(post.replace("-", ""), 80)
 
-    def _decode_mid(self):
+
+class Isan(Upid):
+    """
+    Isan Upid
+    """
+
+    def decode(self):
+        """
+        decode Isan Upid
+        """
+        return self.upid_name, self.bitbin.as_hex(self.bit_length)
+
+    def encode(self, nbin, seg_upid):
+        """
+        encode Isan Upid
+        """
+        nbin.add_hex(seg_upid, (self.upid_length << 3))
+
+
+class Mid(Upid):
+    """
+    Mid Upid
+    """
+
+    def decode(self):
+        """
+        decode Mid Upid
+        """
         upids = []
         ulb = self.bit_length
         while ulb:
@@ -58,7 +171,7 @@ class UpidDecoder:
             ulb -= 8
             upid_length = self.bitbin.as_int(8)
             ulb -= 8
-            upid_type_name, segmentation_upid = UpidDecoder(
+            upid_type_name, segmentation_upid = upid_map[upid_type][1](
                 self.bitbin, upid_type, upid_length
             ).decode()
             mid_upid = {
@@ -69,165 +182,104 @@ class UpidDecoder:
             }
             ulb -= upid_length << 3
             upids.append(mid_upid)
-        return upids
+        return self.upid_name, upids
 
-    def _decode_mpu(self):
+    def encode(self, nbin, seg_upid):
+        """
+        encode Mid Upid
+        """
+        for mid_upid in seg_upid:
+            nbin.add_int(mid_upid["upid_type"], 8)
+            nbin.add_int(mid_upid["upid_length"], 8)
+            the_upid = upid_map[mid_upid["upid_type"]][1](
+                None, mid_upid["upid_type"], mid_upid["upid_length"]
+            )
+            the_upid.encode(nbin, mid_upid["segmentation_upid"])
+
+
+class Mpu(Upid):
+    """
+    Mpu Upid
+    """
+
+    def _decode_adfr(self, mpu_data):
+        """
+        decode_adfr handles Addressabkle TV MPU Upids
+        """
+        data = bytes.fromhex(mpu_data["private_data"][2:])
+        mpu_data["version"] = data[0]
+        mpu_data["channel_identifier"] = hex(int.from_bytes(data[1:3], byteorder="big"))
+        mpu_data["date"] = int.from_bytes(data[3:7], byteorder="big")
+        mpu_data["break_code"] = int.from_bytes(data[7:9], byteorder="big")
+        mpu_data["duration"] = hex(int.from_bytes(data[9:11], byteorder="big"))
+        return mpu_data
+
+    def decode(self):
+        """
+        decode MPU Upids
+        """
         mpu_data = {
             "format_identifier": self.bitbin.as_charset(32),
             "private_data": self.bitbin.as_hex(self.bit_length - 32),
         }
-        if mpu_data["format_identifier"] =="ADFR":
-            data = bytes.fromhex(mpu_data["private_data"][2:])
-            mpu_data["version"] = data[0]
-            mpu_data["channel_identifier"]= hex(int.from_bytes(data[1:3],byteorder="big"))
-            mpu_data["date"] = int.from_bytes(data[3:7],byteorder="big")
-            mpu_data["break_code"]= int.from_bytes(data[7:9],byteorder="big")
-            mpu_data["duration"] =hex(int.from_bytes(data[9:11], byteorder="big"))
-        return mpu_data
+        if mpu_data["format_identifier"] == "ADFR":
+            mpu_data = self._decode_adfr(mpu_data)
+        return self.upid_name, mpu_data
 
-    def _decode_umid(self):
+    def encode(self, nbin, seg_upid):
+        """
+        encode MPU Upids
+        """
+        bit_len = self.bit_length
+        fm = bytes(seg_upid["format_identifier"].encode("utf8"))
+        nbin.add_bites(fm)
+        bit_len -= 32
+        nbin.add_hex(seg_upid["private_data"], bit_len)
+
+
+class Umid(Upid):
+    """
+    Umid Upid
+    """
+
+    def decode(self):
+        """
+        decode Umid Upids
+        """
         chunks = []
         ulb = self.bit_length
         while ulb > 32:
             chunks.append(self.bitbin.as_hex(32)[2:])
             ulb -= 32
-        return ".".join(chunks)
+        return self.upid_name, ".".join(chunks)
 
-    def _decode_uri(self):
-        return self.bitbin.as_charset(self.bit_length, charset)
-
-    def _decode_no(self):
-        return None
-
-    def decode(self):
+    def encode(self, nbin, seg_upid):
         """
-        decode returns a upid determined by
-        self.upid_type and upid_map below.
+        encode Umid Upid
         """
-        upid_map = {
-            0x00: ["No UPID", self._decode_no],
-            0x01: ["Deprecated", self._decode_uri],
-            0x02: ["Deprecated", self._decode_uri],
-            0x03: ["AdID", self._decode_uri],
-            0x04: ["UMID", self._decode_umid],
-            0x05: ["ISAN", self._decode_isan],
-            0x06: ["ISAN", self._decode_isan],
-            0x07: ["TID", self._decode_uri],
-            0x08: ["AiringID", self._decode_air_id],
-            0x09: ["ADI", self._decode_uri],
-            0x10: ["UUID", self._decode_uri],
-            0x11: ["SCR", self._decode_uri],
-            0x0A: ["EIDR", self._decode_eidr],
-            0x0B: ["ATSC", self._decode_atsc],
-            0x0C: ["MPU", self._decode_mpu],
-            0x0D: ["MID", self._decode_mid],
-            0x0E: ["ADS Info", self._decode_uri],
-            0x0F: ["URI", self._decode_uri],
-            0xFD: ["Unknown", self._decode_uri],
-        }
-        if self.upid_type not in upid_map:
-            self.upid_type = 0xFD
-        if not self.upid_length:
-            self.upid_type = 0x00
-        upid_name = upid_map[self.upid_type][0]
-        upid_value = upid_map[self.upid_type][1]()
-        return upid_name, upid_value
+        chunks = seg_upid.split(".")
+        for chunk in chunks:
+            nbin.add_hex(chunk, 32)
 
 
-def upid_encoder(nbin, upid_type, upid_length, seg_upid):
-    """
-    upid_encoder
-    encodes segmentation_upid values to bites
-    added to the nbin instance.
-
-    Used by the SegmentationDescriptor class.
-    """
-    upid_map = {
-        0x00: ["No UPID", _encode_no],
-        0x01: ["Deprecated", _encode_uri],
-        0x02: ["Deprecated", _encode_uri],
-        0x03: ["AdID", _encode_uri],
-        0x04: ["UMID", _encode_umid],
-        0x07: ["TID", _encode_uri],
-        0x0B: ["ATSC", _encode_atsc],
-        0x09: ["ADI", _encode_uri],
-        0x10: ["UUID", _encode_uri],
-        0x11: ["SCR", _encode_uri],
-        0x0A: ["EIDR", _encode_eidr],
-        0x0D: ["MID", _encode_mid],
-        0x0E: ["ADS Info", _encode_uri],
-        0x0F: ["URI", _encode_uri],
-    }
-    if not seg_upid or len(seg_upid) < 1:
-        return
-
-    if upid_type in upid_map:
-        upid_map[upid_type][1](nbin, seg_upid)
-
-    upid_map_too = {
-        0x05: ["ISAN", _encode_isan],
-        0x06: ["ISAN", _encode_isan],
-        0x08: ["AiringID", _encode_air_id],
-        0x0C: ["MPU", _encode_mpu],
-    }
-
-#    if seg_upid and len(seg_upid) > 0:
-    if upid_type in upid_map_too:
-        upid_map_too[upid_type][1](nbin, seg_upid, upid_length)
-
-
-def _encode_air_id(nbin, seg_upid, upid_length):
-    nbin.add_hex(seg_upid, (upid_length << 3))
-
-
-def _encode_atsc(nbin, seg_upid):
-    nbin.add_int(seg_upid["TSID"], 16)
-    nbin.add_int(seg_upid["reserved"], 2)
-    nbin.add_int(seg_upid["end_of_day"], 5)
-    nbin.add_int(seg_upid["unique_for"], 9)
-    nbin.add_bites(seg_upid["content_id"].encode("utf-8"))
-
-
-def _encode_eidr(nbin, seg_upid):
-    pre, post = seg_upid[3:].split("/", 1)
-    nbin.add_int(int(pre), 16)
-    nbin.add_hex(post.replace("-", ""), 80)
-
-
-def _encode_isan(nbin, seg_upid, upid_length):
-    nbin.add_hex(seg_upid, (upid_length << 3))
-
-
-def _encode_mid(nbin, seg_upid):
-    for mid_upid in seg_upid:
-        nbin.add_int(mid_upid["upid_type"], 8)
-        nbin.add_int(mid_upid["upid_length"], 8)
-        upid_encoder(
-            nbin,
-            mid_upid["upid_type"],
-            mid_upid["upid_length"],
-            mid_upid["segmentation_upid"],
-        )
-
-
-def _encode_mpu(nbin, seg_upid, upid_length):
-    bit_len = upid_length << 3
-    nbin.add_int(seg_upid["format_identifier"], 32)
-    bit_len -=32
-    nbin.add_hex(seg_upid["private_data"],bit_len)
-
-
-def _encode_no(nbin, seg_upid):
-    nbin.forward(0)
-
-
-def _encode_umid(nbin, seg_upid):
-    chunks = seg_upid.split(".")
-    for chunk in chunks:
-        nbin.add_hex(chunk, 32)
-
-
-def _encode_uri(nbin, seg_upid):
-    if seg_upid:
-        seg_upid = seg_upid.encode()
-        nbin.add_bites(seg_upid)
+upid_map = {
+    0x00: ["No UPID", NoUpid],
+    0x01: ["Deprecated", Upid],
+    0x02: ["Deprecated", Upid],
+    0x03: ["AdID", Upid],
+    0x04: ["UMID", Umid],
+    0x05: ["ISAN", Isan],
+    0x06: ["ISAN", Isan],
+    0x07: ["TID", Upid],
+    0x08: ["AiringID", AirId],
+    0x09: ["ADI", Upid],
+    0x10: ["UUID", Upid],
+    0x11: ["SCR", Upid],
+    0x0A: ["EIDR", Eidr],
+    0x0B: ["ATSC", Atsc],
+    0x0C: ["MPU", Mpu],
+    0x0D: ["MID", Mid],
+    0x0E: ["ADS Info", Upid],
+    0x0F: ["URI", Upid],
+    0xFD: ["Unknown", Upid],
+}
