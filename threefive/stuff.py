a70a19546b7d7836f4a58374fb1d49a4cf054ba6
Adrian of Doom <52701496+futzu@users.noreply.github.com>
2024-09-21 02:16:59 -0400

    camel_case(),  convert_xml_value() functions

--- threefive/stuff.py
+++ threefive/stuff.py
@@ -10,3 +10,40 @@ def print2(stuff=b""):
     print2 prints to 2 aka stderr.
     """
     print(stuff, file=stderr, flush=True)
+
+
+def camel_case(k):
+    """
+    camel_case changes camel case xml names
+    to underscore_format names.
+    """
+    k = "".join([f"_{i.lower()}" if i.isupper() else i for i in k])
+    return (k, k[1:])[k[0] == "_"]
+
+
+def convert_xml_value(v):
+    """
+    convert_xml_value converts an xml value
+    to ints, floats and booleans.
+    """
+    if v.isdigit():
+        return int(v)
+    if v.replace(".", "").isdigit():
+        return float(v)
+    if v in ["false", "False"]:
+        return False
+    if v in ["true", "True"]:
+        return True
+    return v
+"""
+stuff.py functions and such common to threefive.
+"""
+
+from sys import stderr
+
+
+def print2(stuff=b""):
+    """
+    print2 prints to 2 aka stderr.
+    """
+    print(stuff, file=stderr, flush=True)
