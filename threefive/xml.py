e69a014bb889a8e93ccfc4d47d78cd2303b4e862
Adrian of Doom <52701496+futzu@users.noreply.github.com>
2024-09-26 01:58:44 -0400

    Node.__repr__ set to Node.mk() | Node instance can now be printed

--- threefive/xml.py
+++ threefive/xml.py
@@ -3,28 +3,12 @@ xml.py  The Node class for converting to xml
 and several conversion functions for names and values.
 """
 
-
-def num2xml(val):
-    """
-    num2xml py num to xml num
-    """
-    return str(val)
-
-
-def bool2xml(val):
-    """
-    bool2xml py boolean
-    to xml boolean
-    """
-    return str(val).lower()
-
-
 def val2xml(val):
     """
     val2xmlconvert val for xml
     """
-    if isinstance(val, bool):   return bool2xml(val)
-    if isinstance(val, (int, float)):   return num2xml(val)
+    if isinstance(val, bool):   return str(val).lower()
+    if isinstance(val, (int, float)):   return str(val)
     return val
 
 
@@ -37,21 +21,12 @@ def key2xml(string):
     return new_string[0].lower() + new_string[1:]
 
 
-def mk_attrs(attrs):
+def mk_xml_attrs(attrs):
     """
     mk_attrs converts a dict into
     a dict of xml friendly keys and values
     """
-    return {key2xml(k): val2xml(v) for k, v in attrs.items()}
-
-
-def unroll_attrs(attrs):
-    """
-    unroll_attrs converts attrs
-    into a string for a xml node
-    """
-    return "".join([f' {k}="{v}"' for k, v in attrs.items()])
-
+    return "".join([f' {key2xml(k)}="{val2xml(v)}"' for k, v in attrs.items()])
 
 
 class Node:
@@ -61,16 +36,9 @@ class Node:
     An instance of Node has:
 
         name :      <name> </name>
-
         attrs :     <name attrs[k]="attrs[v]">
-
         value  :    <name>value</name>
-
         children :  <name><children[0]></children[0]</name>
-
-        end :       if end: <name></name>
-                    if not end: <name/>
-
         depth:      tab depth for printing (automatically set)
 
     Use like this:
@@ -81,16 +49,13 @@ class Node:
         st = Node('scte35:SpliceTime',attrs={'pts_time':3442857000})
         ts.add_child(st)
         print(ts)
-
     """
 
     def __init__(self, name, value=None, attrs={}):
         self.name = name
         self.value = value
-        self.attrs = mk_attrs(attrs)
+        self.attrs = attrs
         self.children = []
-        self.end = False
-        if self.value:  self.end = True
         self.depth = None
 
     def __repr__(self):
@@ -106,14 +71,13 @@ class Node:
 
     def mk(self, obj=None):
         """
-        mk makes the node obj,
-        and it's children into
-        an xml representation.
+        mk makes the node obj
+        and it's children into xml.
         """
         if obj is None: obj=self
         obj.set_depth()
         ndent = "   " * obj.depth
-        new_attrs = unroll_attrs(obj.attrs)
+        new_attrs = mk_xml_attrs(obj.attrs)
         rendrd = f"{ndent}<{obj.name}{new_attrs}>"
         if obj.value:   return f"{rendrd}{obj.value}</{obj.name}>\n"
         rendrd = f"{rendrd}\n"
@@ -126,4 +90,3 @@ class Node:
         add_child adds a child node
         """
         self.children.append(child)
-        self.end = True
